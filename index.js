148979fdf8147999b1c99ec1122e72888194ddd1
its-pablo <itspablo@null.net>
2023-03-16 14:56:24 -0700

    More stuff

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-import connect from "./functions/connect"
-import sendChat from "./functions/sendChat"
+import connect from "./functions/connect.js"
+import sendChat from "./functions/sendChat.js"
 
 export default class Craftbot {
     constructor(){
         // uh nothing yet
     }
     async connect(server) {
+        console.log("Connecting to " + server)
         this.SOCKET = await connect(server)
+        console.log( server + " connection finished")
     }
     async sendChat(msg) {
         this.SOCKET.send(await sendChat(msg))
     }
 }
+
+let thing = new Craftbot
+await thing.connect("ffa1")
+await thing.sendChat("yo")
